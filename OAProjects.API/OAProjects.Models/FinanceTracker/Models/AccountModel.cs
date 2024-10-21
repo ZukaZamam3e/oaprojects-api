9041b799086b8664887f801b8fa92720ffd0f78a
ZukaZamam3e <aoswalt91@gmail.com>
2024-10-20 21:02:17 -0400

    Added more

---
+++ OAProjects.API/OAProjects.Models/FinanceTracker/Models/AccountModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OAProjects.Models.FinanceTracker.Models;
+public class AccountModel
+{
+    public int AccountId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string AccoutnName { get; set; }
+
+    public bool DefaultIndc { get; set; }
+}
